3f674ae43d84138305df223ecec10abc524eb2c4
saidi <saidisaihan@gmail.com>
2024-04-13 17:40:45 +0100

    added description page

--- Pages/Pie/pie.js
+++ Pages/Pie/pie.js
@@ -6,10 +6,26 @@ addDataToHtml = (value) => {
   value.forEach((item) => {
     var newFood = document.createElement("div");
     newFood.classList.add("food");
-    newFood.innerHTML = `<img src="/${item.image}" alt="${item.alt}" /><button class="add-to-cart">ADD TO CART</button><div class="sub"><h2 id="food-name">${item.name}</h2><div id="price">${item.price}</div></div>`;
+    newFood.innerHTML = `<img src="/${item.image}" alt="${item.alt}" />
+    <button class="add-to-cart">ADD TO CART</button>
+    <div class="sub">
+    <h2 id="food-name" onclick="openDetailsInNewTab(${item.id})">${item.name}</h2>
+    <div id="price">${item.price}</div></div>`;
     shopingList.appendChild(newFood);
   });
 };
+
+function openDetailsInNewTab(itemId) {
+  const newWindow = window.open("/Pages/Description/description.html");
+  newWindow.onload = function () {
+    const item = foodList.find((i) => i.id === itemId);
+    if (item) {
+      var content = document.createElement("div");
+      content.innerHTML = `<img src="/${item.image}" alt="${item.alt}"><p>${item.description}</p><button></button>`;
+      this.document.getElementById("description").appendChild(content);
+    }
+  };
+}
 
 initApp = () => {
   fetch("/assets/json/data.json")
