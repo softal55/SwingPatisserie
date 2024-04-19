0c5f7fb407a13962dac958d8b07d2bda21cc258f
DESKTOP-SQ424HQ\Sofiane <str14.don@gmail.com>
2024-04-19 04:15:28 +0100

    Our project

--- Pages/Pie/pie.js
+++ Pages/Pie/pie.js
@@ -1,23 +1,8 @@
 let shopingList = document.getElementById("shoping-list");
 
-let foodList = [];
-var cart = [];
-function addToCart(item) {
-  
-  cart.push(item);
-  const total = cart.reduce((acc, curr) => acc + parseFloat(curr.price), 0);
-  
-  // Store cart items in localStorage
-  localStorage.setItem('cart', JSON.stringify(cart));
-  localStorage.setItem('total', total);
 
-  const cartWindow = window.open('/Pages/Cart/cart.html');
-  cartWindow.onload = function() {
-    // Retrieve cart items from localStorage
-    const storedCart = JSON.parse(localStorage.getItem('cart'));
-    const storedTotal = parseFloat(localStorage.getItem('total'));
-    this.initCart(storedCart, storedTotal);
-  };
+function TakeAnOrder() {
+   window.open('/Pages/Order/Order.html');
 }
 
 addDataToHtml = (value) => {
@@ -27,13 +12,13 @@ addDataToHtml = (value) => {
 
     newFood.innerHTML = `
 <img src="/${item.image}" alt="${item.alt}" />
-<button class="add-to-cart">ADD TO CART</button>
+<button class="add-to-cart">Take an order</button>
 <div class="sub">
   <h2 id="food-name" onclick="openDetailsInNewTab(${item.id})">${item.name}</h2>
   <div id="price">${item.price} DA</div>
 </div>`;
 
-    newFood.querySelector('.add-to-cart').addEventListener('click', () => addToCart(item));
+    newFood.querySelector('.add-to-cart').addEventListener('click', () => TakeAnOrder(item));
 
     shopingList.appendChild(newFood);
   });
@@ -44,7 +29,7 @@ function openDetailsInNewTab(itemId) {
   newWindow.onload = function () {
     const item = foodList.find((i) => i.id === itemId);
     if (item) {
-      this.document.getElementById("description").innerHTML = `<img id="food-img" src="/${item.image}" alt="${item.alt}"><p id="food-desc">${item.description} <h2>${item.price} Da</h2></p> <button>Add To Cart</button>`;
+      this.document.getElementById("description").innerHTML = `<img id="food-img" src="/${item.image}" alt="${item.alt}"><p id="food-desc">${item.description} <h2>${item.price} Da</h2></p>`;
     }
   };
 }
